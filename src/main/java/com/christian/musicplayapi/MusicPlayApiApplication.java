295533acf7a297cb7811ea19db8c075330e3eada
Christian Bechstedt <chrisnebech@hotmail.com>
2024-04-24 15:46:47 -0300

    first commit

---
+++ src/main/java/com/christian/musicplayapi/MusicPlayApiApplication.java
@@ -0,0 +1,13 @@
+package com.christian.musicplayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPlayApiApplication.class, args);
+	}
+
+}
