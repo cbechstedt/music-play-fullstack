0c2493568f7eec49e3167bcae00df712199d94d1
Christian Bechstedt <chrisnebech@hotmail.com>
2024-07-03 15:40:02 -0300

    changed frontend url - hosting frontend on vercel, back on render and db on neon

--- app/backend/src/main/java/com/christian/musicplayapi/WebConfig.java
+++ app/backend/src/main/java/com/christian/musicplayapi/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:5173") // URL frontend
+        .allowedOrigins("https://music-play-fullstack.vercel.app") // URL frontend
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
