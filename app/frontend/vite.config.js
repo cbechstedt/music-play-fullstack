7cdc7fc98c801c7f4b3062268b884dd607c9f578
Christian Bechstedt <chrisnebech@hotmail.com>
2024-10-16 14:16:02 -0300

    fix light mode

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '/api': {
         target: process.env.VITE_API_BASE_URL || 'http://localhost:8080',
         changeOrigin: true,
-        secure: false
+        secure: false,
       }
     }
   }
